e1cc3f82dac10df1d8280fe3da6efa438612bb76
Segun <shegunolanitori@gmail.com>
2020-10-11 00:30:15 +0100

    feat: added pm2 package for proess management

--- src/resolvers/queries/products.js
+++ src/resolvers/queries/products.js
@@ -10,11 +10,9 @@ async function products(_, {
     pool
 }) {
     try {
-        const start = Date.now()
+        // const start = Date.now()
         const users = await pool.query(`select * from products order by created_at desc limit ${limit}`)
-        if (users) {
-            console.log(Date.now() - start);
-        }
+        //     console.log(Date.now() - start);
         return users.rows
 
     } catch (err) {
