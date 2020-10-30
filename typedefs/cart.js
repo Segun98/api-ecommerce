a965530fd96e015d5dbe067eaa08ce6a915f4f9f
Segun <shegunolanitori@gmail.com>
2020-10-30 23:34:51 +0100

    feat: minor change

--- typedefs/cart.js
+++ typedefs/cart.js
@@ -4,12 +4,12 @@ const {
 
 module.exports = gql `
     type cart{
-    id: ID,
-    quantity: Int,
-    product_id: ID,
-    prod_creator_id: ID,
-    customer_id: ID,
-    created_at: String,
+    id: ID
+    quantity: Int
+    product_id: ID
+    prod_creator_id: ID
+    customer_id: ID
+    created_at: String
 
     #nested query
     cartCreator:usersRes
