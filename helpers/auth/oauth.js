8019246feca4c06d2d571b34fc3e270e89b48dcc
Segun <shegunolanitori@gmail.com>
2020-11-09 16:37:40 +0100

    feat : combined sign up and login at oauth route

--- helpers/auth/oauth.js
+++ helpers/auth/oauth.js
@@ -1,3 +1,4 @@
+//@ts-check
 const router = require("express").Router()
 const pool = require("../../db")
 const {
@@ -13,41 +14,64 @@ const bcrypt = require("bcryptjs")
 router.post("/oauth/login", async (req, res) => {
 
     const {
+        first_name,
+        last_name,
+        password,
         email
     } = req.body
+
 
     try {
         const users = await pool.query("select * from users where email = $1", [email]);
 
-        //user that doesn't exist
-        if (users.rows.length === 0) {
-            return res.status(404).send('User does not exist')
+        //user exists, login
+        if (users.rows.length > 0) {
+            //imports to create tokens
+            const token = createToken(users) //returns access token
+
+            res.send({
+                refreshtoken: createRefreshToken(users),
+                accesstoken: token,
+                role: users.rows[0].role
+            })
+        } else {
+            //else sign them up
+
+            // hash password , in this case, user id from google
+
+            const salt = await bcrypt.genSalt(10)
+            const hashedpassword = await bcrypt.hash(password, salt)
+            //Password is google user id to track people signing up with google
+            await pool.query(`INSERT INTO users (first_name,last_name,email,password,phone,role,pending,business_name,
+                            business_name_slug,
+                            business_address,
+                            business_image,
+                            business_bio,
+                            customer_address) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`,
+                [first_name, last_name, email, hashedpassword, null, 'customer', 'false', null,
+                    null,
+                    null,
+                    null,
+                    'Google',
+                    null
+                ]);
+
+            //welcome email
+            await welcomeCustomer(first_name, email)
+
+            //get the new user from DB
+
+            const user = await pool.query("select * from users where email = $1", [email]);
+
+            const token = createToken(user) //returns access token
+
+            return res.send({
+                refreshtoken: createRefreshToken(user),
+                accesstoken: token,
+                role: user.rows[0].role
+            })
         }
-        // cookie expiry date - 7 days 
-        // let date = new Date()
-        // date.setDate(date.getDate() + 7);
 
-        //imports to create tokens
-        const token = createToken(users) //returns access token
-
-        /* 
-        COOKIE NOT SETTING(VERCEL AND HEROKU PROBLEMS) SO I'D DO IT CLIENT SIDE :-(
-        */
-        // res.cookie('ecom', createRefreshToken(users), {
-        //     // httpOnly: true,
-        //     expires: date,
-        //     // secure: true,
-        // })
-        // //role here is either customer, admin or vendor. it's determined at sign up
-        // res.cookie('role', users.rows[0].role, {
-        //     expires: date,
-        // });
-
-        res.send({
-            refreshtoken: createRefreshToken(users),
-            accesstoken: token,
-            role: users.rows[0].role
-        })
 
     } catch (err) {
         res.send(err.message)
