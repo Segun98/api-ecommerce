c5b484c38e52a465df6bfbe1457b276a18e40bf6
Segun <shegunolanitori@gmail.com>
2020-12-08 18:40:56 +0100

    feat: minor change

--- resolvers/mutations/admin/users.js
+++ resolvers/mutations/admin/users.js
@@ -14,7 +14,7 @@ async function updateCompleted(_, {
 
     try {
         //fetch a vendor's completed orders
-        const completed_orders = await pool.query(`select completed, vendor_email from orders where completed = $2 and prod_creator_id= $1`, [id, 'true'])
+        const completed_orders = await pool.query(`select completed from orders where completed = $2 and prod_creator_id= $1`, [id, 'true'])
         //get the length
         let completed_orders_length = completed_orders.rows.length
         //update users table with the length
