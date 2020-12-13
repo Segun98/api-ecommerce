743944587b85864bc5fd62541a5604b39b6b1e37
Segun <shegunolanitori@gmail.com>
2020-12-13 18:22:44 +0100

    remove products that are out of stock

--- resolvers/nested-queries/productsRes.js
+++ resolvers/nested-queries/productsRes.js
@@ -14,8 +14,7 @@ async function related(parent, {}, {
     pool
 }) {
     try {
-        const result = await pool.query(`select p.id, p.name, p.name_slug, p.price, p.images from products p inner join users u on p.creator_id = u.id where p.category = $1 order by u.completed_qty desc limit 8`, [parent.category])
-        //remember to filter out duplicate store ownwesr
+        const result = await pool.query(`select p.id, p.name, p.name_slug, p.price, p.images from products p inner join users u on p.creator_id = u.id where p.category = $1 and p.available_qty > 0 and p.in_stock = $2 and u.online = $2 order by u.completed_qty desc limit 8`, [parent.category, "true"])
         return result.rows
 
     } catch (err) {
