3a7415e824a842a3d13f85f40ae6b60452f4fafa
Segun <shegunolanitori@gmail.com>
2021-01-07 11:05:17 +0100

    Feat: getOrderStatus to fetch paid for orders for specofoc data in dashboard

--- resolvers/queries/admin/orders.js
+++ resolvers/queries/admin/orders.js
@@ -19,10 +19,31 @@ module.exports = {
             throw new Error("Unauthorised, you are not an admin")
         }
         try {
-            const result = await pool.query(`select * from orders where paid = $1 order by created_at desc`, ['true'])
+            const result = await pool.query(`SELECT * from orders ORDER BY created_at desc`)
             return result.rows
         } catch (err) {
             throw new Error(err.message)
         }
+    },
+
+    async getOrderStatus(_, {}, {
+        pool,
+        req
+    }) {
+        verifyJwt(req)
+        const {
+            role_id
+        } = req.payload
+
+        if (!(role_id === "admin" || role_id === "super_admin")) {
+            throw new Error("Unauthorised, you are not an admin")
+        }
+        try {
+            const result = await pool.query(`select * from order_status where paid = $1 order by created_at desc`, ['true'])
+            return result.rows
+        } catch (err) {
+            throw new Error(err.message)
+        }
+
     }
 }
